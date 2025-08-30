76494d1857fbf335f6a2de4b2ee275694093a95e
Balbhadra Daxini <balbhadradaxini830376@gmail.com>
2025-08-30 11:07:35 +0000

    chnaged the token but still not working. below is the token
    Storefront

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -1,12 +1,9 @@
 
 import { GraphQLClient, gql } from 'graphql-request';
 import type { Product, ShopifyProductVariant, Collection } from './types';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_API_TOKEN;
-const endpoint = `https://${process.env.SHOPIFY_STORE_DOMAIN}/api/2023-10/graphql.json`;
+const endpoint = `https://://${process.env.SHOPIFY_STORE_DOMAIN}/api/2023-10/graphql.json`;
 
 let client: GraphQLClient | null = null;
 
